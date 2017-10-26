f5b2045c7885397ed15e10ffbe58a01cbf44b983
Matt Thomas <matt.thomas1202@yahoo.com>
2017-10-26 00:05:43 +0000

    Added user profiles form

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,8 @@
+class ProfilesController < ApplicationController
+  
+  # GET to /user/:user_id/profile/new
+  def new
+    # Render blank profile details form
+    @profile = Profile.new
+  end
+end
